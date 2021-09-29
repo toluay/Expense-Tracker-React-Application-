adbd361f07127c5943a7474c5808f222dd7f4d0a
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-29 17:52:39 +0100

    { useContext } imported in the list component

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  { useContext } from 'react';
 import { List as MUIList, ListItem, ListItemAvatar, Avatar, ListItemText, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
 import { Delete, MoneyOff } from '@material-ui/icons';
 import { ExpenseTrackerContext } from '../../../context/context';
