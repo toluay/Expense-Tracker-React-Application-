cf4a8cb93f37c270dbb377d49625440c6946c1cd
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-29 17:34:49 +0100

    balance variate is created in the context and imported to the main componet to get the total balance

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,19 @@
-import React  from 'react';
+import React , {  useContext }  from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import { ExpenseTrackerContext } from '../../context/context';
 import useStyles from './styles';
 import Form from './Form/Form';
 import List from './List/List';
 
 const ExpenseTracker = () => {
     const classes = useStyles();
+    const { balance } = useContext(ExpenseTrackerContext);
 
     return (
         <Card className={classes.root}>
           <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
           <CardContent>
-            <Typography align="center" variant="h5">Total Balance $100</Typography>
+            <Typography align="center" variant="h5">`Total Balance N${balance}`</Typography>
             <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
               {/* <InfoCard /> */} infocard gets here 
             </Typography>
