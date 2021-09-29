5886e04cbac5e4991e232fa6299d32530b56606d
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-29 17:26:55 +0100

    usestate useeffect  use speechcontext usesate used too get values fron the speechley application to the application to get the text of the speech and used in the application

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,7 +1,12 @@
-import React, { useState,  useContext,} from  'react';
+import React, { useState,  useContext,useEffect } from  'react';
 import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 import { v4 as uuidv4 } from 'uuid';
+import Snackbar from '../../Snackbar/Snackbar';
+
+import { useSpeechContext } from '@speechly/react-client';
+
 import formatDate from '../../../utils/formatDate';
+const { segment } = useSpeechContext();
 import { ExpenseTrackerContext } from '../../../context/context';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
 import useStyles from './styles';
@@ -19,20 +24,77 @@ const NewTransactionForm = () => {
     const classes = useStyles();
     const { addTransaction } = useContext(ExpenseTrackerContext);
     const [formData, setFormData] = useState(initialState);
+    const { segment } = useSpeechContext();
+    const [open, setOpen] = React.useState(false);
 
     const createTransaction = () => {
+        if (Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
+        if (incomeCategories.map((iC) => iC.type).includes(formData.category)) {
+            setFormData({ ...formData, type: 'Income' });
+          } else if (expenseCategories.map((iC) => iC.type).includes(formData.category)) {
+            setFormData({ ...formData, type: 'Expense' });
+          }
+      
+          setOpen(true);
+
         
         addTransaction({ ...formData, amount: Number(formData.amount), id: uuidv4() });
         setFormData(initialState);
       };
 
+      useEffect(() => {
+        if (segment) {
+          if (segment.intent.intent === 'add_expense') {
+            setFormData({ ...formData, type: 'Expense' });
+          } else if (segment.intent.intent === 'add_income') {
+            setFormData({ ...formData, type: 'Income' });
+          } else if (segment.isFinal && segment.intent.intent === 'create_transaction') {
+            return createTransaction();
+          } else if (segment.isFinal && segment.intent.intent === 'cancel_transaction') {
+            return setFormData(initialState);
+          }
+    
+          segment.entities.forEach((s) => {
+            const category = `${s.value.charAt(0)}${s.value.slice(1).toLowerCase()}`;
+    
+            switch (s.type) {
+              case 'amount':
+                setFormData({ ...formData, amount: s.value });
+                break;
+              case 'category':
+                if (incomeCategories.map((iC) => iC.type).includes(category)) {
+                  setFormData({ ...formData, type: 'Income', category });
+                } else if (expenseCategories.map((iC) => iC.type).includes(category)) {
+                  setFormData({ ...formData, type: 'Expense', category });
+                }
+                break;
+              case 'date':
+                setFormData({ ...formData, date: s.value });
+                break;
+              default:
+                break;
+            }
+          });
+    
+          if (segment.isFinal && formData.amount && formData.category && formData.type && formData.date) {
+            createTransaction();
+          }
+        }
+      }, [segment]);
+    
+
     const selectedCategories = formData.type === 'Income' ? incomeCategories : expenseCategories;
     return (
         <Grid container spacing={2}>
-      
+       <Snackbar open={open} setOpen={setOpen} />
           <Grid item xs={12}>
             <Typography align="center" variant="subtitle2" gutterBottom>
-            To create and expense !!!
+            {/* To create and expense !!! */}
+            {segment ? (
+        <div className="segment">
+          {segment.words.map((w) => w.value).join(" ")}
+        </div>
+      ) : null}
             </Typography>
           </Grid>
           <Grid item xs={6}>
