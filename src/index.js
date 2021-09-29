e00f3af76a2b4330ac2969d24aa2e7b88d57b549
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-29 15:22:06 +0100

    usecontext and contextreducer created and imported into the index.js as proveider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from './context/context';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-    <App />,  document.getElementById('root'),
+    <Provider> 
+        <App />
+    </Provider>, 
+     document.getElementById('root'),
     );
