ff8db7dbdd7aca28b28995ccb90fc27cbee18390
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-29 16:36:40 +0100

    Doughnut component imported and used with the usetransaction ook to get the reqiured data

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 
 import useStyles from './styles';
+import useTransactions from '../../useTransactions';
 
 const DetailsCard = ({ title, subheader }) => {
+    const { total, chartData } = useTransactions(title);
     const classes = useStyles();
 
     return (<Card className={title === 'Income' ? classes.income : classes.expense}>
         <CardHeader title={title} subheader={subheader} />
       <CardContent>
-        <Typography variant="h5">$100</Typography>
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
        
       </CardContent>
 
