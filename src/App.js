8bcb9de671cb3eae9603d00cb8502c00269ddf7e
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-29 14:41:20 +0100

    main app component added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useRef } from 'react';
+import { Grid } from '@material-ui/core';
+
+const App = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+
+
+    </div>
+  );
+};
+
+export default App;
