f7b0ff99f2415bb75c9e2d6321646d73c36a0593
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-29 14:40:55 +0100

    Detail component added with sytylejs

---
+++ src/components/Details/Details.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const DetailsCard = ({ title, subheader }) => {
+    const classes = useStyles();
+
+    return (<Card>
+        <CardHeader title={title} subheader={subheader} />
+      <CardContent>
+        <Typography variant="h5">$100</Typography>
+       
+      </CardContent>
+
+        </Card>
+        );
+
+};
+
+export default DetailsCard;
