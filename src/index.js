09e26ff0831c01b51f84a23142c123928c6534f8
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-29 17:12:52 +0100

    PushToTalkButton, PushToTalkButtonContainer added to index js and provider added for sppeechly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from './context/context';
+import { SpeechProvider } from '@speechly/react-client';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
+    <SpeechProvider appId="693a5d25-4bdf-47a6-bd87-277a1eeb138d" language="en-US">
     <Provider> 
         <App />
-    </Provider>, 
+    </Provider>
+    </SpeechProvider>, 
      document.getElementById('root'),
     );
