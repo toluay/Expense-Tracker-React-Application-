f1d54578788a38ee6915567f6d8024a0eadb7607
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-29 16:23:11 +0100

    context function  imported to list component and use

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { List as MUIList, ListItem, ListItemAvatar, Avatar, ListItemText, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
 import { Delete, MoneyOff } from '@material-ui/icons';
+import { ExpenseTrackerContext } from '../../../context/context';
 
 import useStyles from './styles';
 
 const List = () => {
     const classes = useStyles();
+    const { transactions, deleteTransaction } = useContext(ExpenseTrackerContext);
 
-    const transactions = [{
-        'id':1,
-        "type":"income",
-        "categories": "Salary",
-        "amount": 50,
-        "date":"wed Dec 2022"
-    }, {
-        'id':2,
-        "type":"Expense ",
-        "categories": "Fuel ",
-        "amount": 500,
-        "date":"wed Dec 2022"
-    }];
+    // const transactions = [{
+    //     'id':1,
+    //     "type":"income",
+    //     "categories": "Salary",
+    //     "amount": 50,
+    //     "date":"wed Dec 2022"
+    // }, {
+    //     'id':2,
+    //     "type":"Expense ",
+    //     "categories": "Fuel ",
+    //     "amount": 500,
+    //     "date":"wed Dec 2022"
+    // }];
 
     return (
         <MUIList dense={false} className={classes.list}>
@@ -33,7 +35,7 @@ const List = () => {
                 </ListItemAvatar>
                 <ListItemText primary={transaction.category} secondary={`N${transaction.amount} - ${transaction.date}`} />
                 <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="delete" onClick="">
+                  <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
                     <Delete />
                   </IconButton>
                 </ListItemSecondaryAction>
