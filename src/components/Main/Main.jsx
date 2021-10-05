24c09b712751cf4466e699bd1311f4b06ea0dfd0
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-10-05 16:40:33 +0100

    commited

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,7 +12,7 @@ const ExpenseTracker = () => {
 
     return (
         <Card className={classes.root}>
-          <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+          <CardHeader title="Expense Tracker" subheader="......." />
           <CardContent>
             <Typography align="center" variant="h5">Total Balance <br/> {balance}</Typography>
             <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
